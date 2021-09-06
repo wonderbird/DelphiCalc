3eae4a055a878c4ed48820d1abcbb7c7d06501d3
Stefan Boos <kontakt@boos.systems>
2021-09-06 06:13:24 +0200

    Initial project after following tutorial step 2
    https://community.embarcadero.com/blogs/entry/Learn-DelphiCE-Part2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# DelphiCalc
-Delphi Super Calculator project from https://community.embarcadero.com/blogs/entry/Learn-DelphiCE-Part2
+# Delphi Calc
+
+Delphi Super Calculator project from [Learn to Program with Delphi Community Edition](https://community.embarcadero.com/blogs/entry/Learn-DelphiCE-Part2) and following tutorial pages.
---
+++ uFormCalculator.pas
@@ -0,0 +1,32 @@
+unit uFormCalculator;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Hello from Delphi');
+end;
+
+end.
